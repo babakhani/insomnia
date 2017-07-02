ab5c8961ed1450352be4ba8c576408d7aeaef34d
reza bbakhani <babakhani.reza@gmail.com>
2017-07-03 01:13:51 +0430

    change all native base material theme fonts

--- native-base-theme/variables/material.js
+++ native-base-theme/variables/material.js
@@ -22,7 +22,7 @@ export default {
   badgePadding: (platform === 'ios') ? 3 : 0,
 
     // Button
-  btnFontFamily: (platform === 'ios') ? 'Roboto' : 'Roboto_medium',
+  btnFontFamily: (platform === 'ios') ? 'IRANSans' : 'Roboto_medium',
   btnDisabledBg: '#b5b5b5',
   btnDisabledClr: '#f1f1f1',
 
@@ -265,7 +265,7 @@ export default {
 
 
     // Title
-  titleFontfamily: (platform === 'ios') ? 'Roboto' : 'Roboto_medium',
+  titleFontfamily: (platform === 'ios') ? 'IRANSans' : 'Roboto_medium',
   titleFontSize: 19,
   subTitleFontSize: 14,
   subtitleColor: '#FFF',
